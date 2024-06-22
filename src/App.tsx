26bd152ef29991279719bf9d838ed8e881c41937
Itay Fintz <itayfi@gmail.com>
2024-06-22 15:13:17 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { ComponentProps, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+
+function Box(props: ComponentProps<"mesh">) {
+  const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  return (
+    <mesh
+      {...props}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => setClicked(!clicked)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <Canvas style={{ height: "100dvh" }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
+
+export default App;
